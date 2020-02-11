5aa03a231ec4aa6f78897a232bd48884f879edf3
Paul Gyasi Asamoah <asamoah001@gannon.edu>
2020-02-10 22:44:17 -0500

    Complete calcHourlyWage structure

---
+++ src/assignment1salarycalculator/SalaryCalculator.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1salarycalculator;
+
+/**
+ *
+ * @author PaulGee
+ */
+public class SalaryCalculator {
+    
+    public int calcHourlyWage(int salary){
+        int hourWage = salary/(52*40);
+        System.out.println("Hourly Wage is " + hourWage);
+        return hourWage;
+    }
+    
+    
+}
+
+    
+
